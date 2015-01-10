7014733e325cc08907f7da13fb1fe9d4236a666b
Ross Coleman <rossco85@gmail.com>
2015-01-09 21:28:15 -0600

    initial spring boot setup & .gitignore

---
+++ src/test/java/net/letscarpool/rest/LetscarpoolRestApplicationTests.java
@@ -0,0 +1,16 @@
+package net.letscarpool.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LetscarpoolRestApplication.class)
+public class LetscarpoolRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
