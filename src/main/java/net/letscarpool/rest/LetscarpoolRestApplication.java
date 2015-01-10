a43c0219bbb7247a396575b8b20fd0f610e9bc7d
Ross Coleman <rossco85@gmail.com>
2015-01-09 22:14:01 -0600

    spring boot can auto-configure h2 database

--- src/main/java/net/letscarpool/rest/LetscarpoolRestApplication.java
+++ src/main/java/net/letscarpool/rest/LetscarpoolRestApplication.java
@@ -1,31 +1,15 @@
 package net.letscarpool.rest;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class LetscarpoolRestApplication {
 	
-	@Bean
-    public DataSource dataSource() {
-    	// simple DS for test (not for production!)
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(org.h2.Driver.class);
-        dataSource.setUsername("sa");
-        dataSource.setUrl("jdbc:h2:mem");
-        dataSource.setPassword("");
-        return dataSource;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LetscarpoolRestApplication.class, args);
     }
