3d232a2e118cc5ac73a922de7ae61e0c5e7784c5
Ross Coleman <rossco85@gmail.com>
2015-03-14 17:08:10 -0500

    Project lombok

--- src/main/java/net/letscarpool/rest/domain/Location.java
+++ src/main/java/net/letscarpool/rest/domain/Location.java
@@ -16,69 +16,17 @@
 
 package net.letscarpool.rest.domain;
 
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
 public class Location {
 	private Integer id;
 	private String address;
 	private String city;
 	private String state;
 	private Integer zip;
-	private Double latitude;
-	private Double longitude;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
-
-	public Double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public Integer getZip() {
-		return zip;
-	}
-
-	public void setZip(Integer zip) {
-		this.zip = zip;
-	}
-
+	private BigDecimal latitude;
+	private BigDecimal longitude;
 }
